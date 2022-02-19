1087d2ecea3b834597493b0bceb41d6758f0fdef
MRadzynski <radzynski.milosz@gmail.com>
2022-02-19 18:16:48 +0100

    Adjust styles for all mobiles

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,40 @@ import About from './components/Sections/About/About';
 import Offer from './components/Sections/Offer/Offer';
 import Contact from './components/Sections/Contact/Contact';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import NotAllowedOverlay from './components/NotAllowedOverlay/NotAllowedOverlay';
 import { smoothNavScroll } from './utils/smoothNavScroll';
 import { useEffect, useState } from 'react';
 
 import classes from './styles/components/app.module.scss';
 
+import breakpointsData from './data/breakpoints.json';
+
+const defaultWidth = window.innerWidth;
+
 const App = () => {
 	const [displayArrow, setDisplayArrow] = useState(false);
+	const [noSupportedOrientation, setNoSupportedOrientation] = useState(false);
+
+	const {
+		breakpoints: { tablet },
+	} = breakpointsData;
 
 	useEffect(() => {
 		smoothNavScroll();
 		document.addEventListener('scroll', displayArrowHandler);
+
+		window.screen.orientation.addEventListener('change', (e) => {
+			if (
+				e.currentTarget.type === 'landscape-primary' &&
+				defaultWidth < tablet
+			) {
+				document.querySelector('#app').classList.add(classes.stopScrolling);
+				setNoSupportedOrientation(true);
+			} else {
+				document.querySelector('#app').classList.remove(classes.stopScrolling);
+				setNoSupportedOrientation(false);
+			}
+		});
 
 		return () => document.removeEventListener('scroll', displayArrowHandler);
 	}, []);
@@ -24,7 +47,7 @@ const App = () => {
 		window.scrollY > 500 ? setDisplayArrow(true) : setDisplayArrow(false);
 
 	return (
-		<div className={classes.appContainer}>
+		<div className={classes.appContainer} id="app">
 			<Header />
 			<Welcome />
 			<About />
@@ -32,6 +55,7 @@ const App = () => {
 			<Contact />
 			<Footer />
 			{displayArrow && <ScrollToTop />}
+			{noSupportedOrientation && <NotAllowedOverlay />}
 		</div>
 	);
 };
---
+++ src/components/NotAllowedOverlay/NotAllowedOverlay.jsx
@@ -0,0 +1,13 @@
+import classes from '../../styles/components/notAllowedOverlay.module.scss';
+
+const NotAllowedOverlay = () => {
+	return (
+		<div className={classes.overlayContainer}>
+			<h1 className={classes.notAllowedMessage}>
+				Obróć telefon, aby moć swobodnie korzystać ze strony
+			</h1>
+		</div>
+	);
+};
+
+export default NotAllowedOverlay;
