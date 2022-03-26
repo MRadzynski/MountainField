4ee3e47149293194cf843ad857d8eac6357704f1
MRadzynski <radzynski.milosz@gmail.com>
2022-03-26 19:44:01 +0100

    Adjust styling for ipads

--- src/components/Forms/PlansForm/PlansForm.jsx
+++ src/components/Forms/PlansForm/PlansForm.jsx
@@ -8,129 +8,129 @@ import { useEffect, useState } from 'react';
 const zipCodeRegex = /^[0-9]{2}-[0-9]{3}$/;
 
 const PlansForm = ({
-	data,
-	handleChange,
-	handleChoose,
-	nextStep,
-	prevStep,
+  data,
+  handleChange,
+  handleChoose,
+  nextStep,
+  prevStep,
 }) => {
-	const [isNextButtonDisabled, setIsNextButtonDisabled] = useState(false);
+  const [isNextButtonDisabled, setIsNextButtonDisabled] = useState(false);
 
-	useEffect(() => {
-		if (
-			!data.standardOffer &&
-			data.homeOfficeOffer &&
-			Number(data.workersAmount) > 0
-		) {
-			setIsNextButtonDisabled(false);
-		} else if (
-			!data.homeOfficeOffer &&
-			data.standardOffer &&
-			data.address.city.length > 2 &&
-			data.address.street.length > 2 &&
-			zipCodeRegex.test(data.address.zipCode)
-		) {
-			setIsNextButtonDisabled(false);
-		} else if (
-			data.homeOfficeOffer &&
-			data.standardOffer &&
-			data.address.city.length > 2 &&
-			data.address.street.length > 2 &&
-			zipCodeRegex.test(data.address.zipCode) &&
-			Number(data.workersAmount) > 0
-		) {
-			setIsNextButtonDisabled(false);
-		} else {
-			setIsNextButtonDisabled(true);
-		}
-	}, [data]);
+  useEffect(() => {
+    if (
+      !data.standardOffer &&
+      data.homeOfficeOffer &&
+      Number(data.workersAmount) > 0
+    ) {
+      setIsNextButtonDisabled(false);
+    } else if (
+      !data.homeOfficeOffer &&
+      data.standardOffer &&
+      data.address.city.length > 2 &&
+      data.address.street.length > 2 &&
+      zipCodeRegex.test(data.address.zipCode)
+    ) {
+      setIsNextButtonDisabled(false);
+    } else if (
+      data.homeOfficeOffer &&
+      data.standardOffer &&
+      data.address.city.length > 2 &&
+      data.address.street.length > 2 &&
+      zipCodeRegex.test(data.address.zipCode) &&
+      Number(data.workersAmount) > 0
+    ) {
+      setIsNextButtonDisabled(false);
+    } else {
+      setIsNextButtonDisabled(true);
+    }
+  }, [data]);
 
-	return (
-		<div className={classes.plansContainer}>
-			<h2 className={classes.stepTitle}>WYBIERZ SUBSKRYPCJE</h2>
-			<div
-				className={clsx(classes.planTile, {
-					[classes.pickedPlan]: data.homeOfficeOffer,
-				})}
-			>
-				<h2 className={classes.planTitle}>
-					{window.innerWidth > 1024 && 'Oferta'} HOME OFFICE 🏡
-				</h2>
-				<div className={classes.sliderContainer}>
-					<div className={classes.sliderInfo}>
-						<h3 className={classes.sliderLabel}>Liczba pracowników 👥</h3>
-						<span className={classes.sliderValue}>x{data.workersAmount}</span>
-					</div>
-					<input
-						className={classes.inputSlider}
-						defaultValue={data.workersAmount}
-						max="30"
-						min="0"
-						name="workersAmount"
-						onChange={handleChange}
-						step="1"
-						type="range"
-					/>
-					<p className={classes.inputSliderDescription}>
-						Podaj liczbę pracowników, którzy pracują w trybie zdalnym a co
-						miesiąc otrzymasz daną liczbę kodów do realizacji zamówień na adresy
-						pracowników.
-					</p>
-				</div>
-				<FormButton
-					classes={classes}
-					onClick={() => handleChoose('home')}
-					text="Wybieram"
-				/>
-			</div>
-			<div
-				className={clsx(classes.planTile, {
-					[classes.pickedPlan]: data.standardOffer,
-				})}
-			>
-				<h2 className={classes.planTitle}>
-					{window.innerWidth > 1024 && 'Oferta'} STANDARD 🏢
-				</h2>
-				<div className={classes.inputsContainer}>
-					<FormInput
-						classes={classes}
-						name="city"
-						onChange={handleChange}
-						placeholder="Miasto"
-						title="Miasto"
-						type="text"
-						value={data.address.city}
-					/>
-					<FormInput
-						classes={classes}
-						name="street"
-						onChange={handleChange}
-						placeholder="Ulica i numer lokalu"
-						title="Ulica"
-						type="text"
-						value={data.address.street}
-					/>
-					<FormInput
-						classes={classes}
-						name="zipCode"
-						onChange={handleChange}
-						placeholder="Kod pocztowy 04-457"
-						title="Kod pocztowy"
-						type="text"
-						value={data.address.zipCode}
-					/>
-				</div>
-				<FormButton classes={classes} onClick={handleChoose} text="Wybieram" />
-			</div>
-			<FormButton classes={classes} onClick={prevStep} text="Wróć" />
-			<FormButton
-				classes={classes}
-				disabled={isNextButtonDisabled}
-				onClick={nextStep}
-				text="Dalej"
-			/>
-		</div>
-	);
+  return (
+    <div className={classes.plansContainer}>
+      <h2 className={classes.stepTitle}>WYBIERZ SUBSKRYPCJE</h2>
+      <div
+        className={clsx(classes.planTile, {
+          [classes.pickedPlan]: data.homeOfficeOffer,
+        })}
+      >
+        <h2 className={classes.planTitle}>
+          {window.innerWidth >= 1280 && 'Oferta'} HOME OFFICE 🏡
+        </h2>
+        <div className={classes.sliderContainer}>
+          <div className={classes.sliderInfo}>
+            <h3 className={classes.sliderLabel}>Liczba pracowników 👥</h3>
+            <span className={classes.sliderValue}>x{data.workersAmount}</span>
+          </div>
+          <input
+            className={classes.inputSlider}
+            defaultValue={data.workersAmount}
+            max='30'
+            min='0'
+            name='workersAmount'
+            onChange={handleChange}
+            step='1'
+            type='range'
+          />
+          <p className={classes.inputSliderDescription}>
+            Podaj liczbę pracowników, którzy pracują w trybie zdalnym a co
+            miesiąc otrzymasz daną liczbę kodów do realizacji zamówień na adresy
+            pracowników.
+          </p>
+        </div>
+        <FormButton
+          classes={classes}
+          onClick={() => handleChoose('home')}
+          text='Wybieram'
+        />
+      </div>
+      <div
+        className={clsx(classes.planTile, {
+          [classes.pickedPlan]: data.standardOffer,
+        })}
+      >
+        <h2 className={classes.planTitle}>
+          {window.innerWidth >= 1280 && 'Oferta'} STANDARD 🏢
+        </h2>
+        <div className={classes.inputsContainer}>
+          <FormInput
+            classes={classes}
+            name='city'
+            onChange={handleChange}
+            placeholder='Miasto'
+            title='Miasto'
+            type='text'
+            value={data.address.city}
+          />
+          <FormInput
+            classes={classes}
+            name='street'
+            onChange={handleChange}
+            placeholder='Ulica i numer lokalu'
+            title='Ulica'
+            type='text'
+            value={data.address.street}
+          />
+          <FormInput
+            classes={classes}
+            name='zipCode'
+            onChange={handleChange}
+            placeholder='Kod pocztowy 04-457'
+            title='Kod pocztowy'
+            type='text'
+            value={data.address.zipCode}
+          />
+        </div>
+        <FormButton classes={classes} onClick={handleChoose} text='Wybieram' />
+      </div>
+      <FormButton classes={classes} onClick={prevStep} text='Wróć' />
+      <FormButton
+        classes={classes}
+        disabled={isNextButtonDisabled}
+        onClick={nextStep}
+        text='Dalej'
+      />
+    </div>
+  );
 };
 
 export default PlansForm;
--- src/components/Forms/ProductsForm/ProductsForm.jsx
+++ src/components/Forms/ProductsForm/ProductsForm.jsx
@@ -4,183 +4,183 @@ import FormButton from '../../FormButton/FormButton';
 import { useEffect, useState } from 'react';
 
 const ProductsForm = ({
-	formData,
-	nextStep,
-	prevStep,
-	productsList,
-	setFormData,
-	type,
+  formData,
+  nextStep,
+  prevStep,
+  productsList,
+  setFormData,
+  type,
 }) => {
-	const formDataField = type === 'home' ? 'homeOfficeCart' : 'standardCart';
-	const formDataSum = type === 'home' ? 'homeOfficeSum' : 'standardSum';
-	const headingText = type === 'home' ? 'Home Office' : 'Standard';
-	const subHeadingText = type === 'home' ? 'Na osobę' : 'Do Twojego biura';
+  const formDataField = type === 'home' ? 'homeOfficeCart' : 'standardCart';
+  const formDataSum = type === 'home' ? 'homeOfficeSum' : 'standardSum';
+  const headingText = type === 'home' ? 'Home Office' : 'Standard';
+  const subHeadingText = type === 'home' ? 'Na osobę' : 'Do Twojego biura';
 
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [cartList, setCartList] = useState(formData[formDataField]);
-	const [summaryData, setSummaryData] = useState({
-		coffee: 0,
-		tea: 0,
-		mate: 0,
-		sum: 0,
-	});
-	useEffect(() => {
-		setCartList(formData[formDataField]);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [type]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [cartList, setCartList] = useState(formData[formDataField]);
+  const [summaryData, setSummaryData] = useState({
+    coffee: 0,
+    tea: 0,
+    mate: 0,
+    sum: 0,
+  });
+  useEffect(() => {
+    setCartList(formData[formDataField]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [type]);
 
-	useEffect(() => {
-		const newSummaryData = cartList.reduce(
-			(acc, cartItem) => {
-				acc[cartItem.category] += cartItem.quantity;
-				acc.sum += Number((cartItem.quantity * cartItem.price).toFixed(2));
-				acc.sum = Number(acc.sum.toFixed(2));
+  useEffect(() => {
+    const newSummaryData = cartList.reduce(
+      (acc, cartItem) => {
+        acc[cartItem.category] += cartItem.quantity;
+        acc.sum += Number((cartItem.quantity * cartItem.price).toFixed(2));
+        acc.sum = Number(acc.sum.toFixed(2));
 
-				return acc;
-			},
-			{
-				coffee: 0,
-				tea: 0,
-				mate: 0,
-				sum: 0,
-			}
-		);
+        return acc;
+      },
+      {
+        coffee: 0,
+        tea: 0,
+        mate: 0,
+        sum: 0,
+      }
+    );
 
-		setSummaryData(newSummaryData);
-		setFormData((prevData) => ({
-			...prevData,
-			[formDataField]: cartList,
-			[formDataSum]: newSummaryData.sum,
-		}));
-	}, [cartList, formDataField, formDataSum, setFormData]);
+    setSummaryData(newSummaryData);
+    setFormData((prevData) => ({
+      ...prevData,
+      [formDataField]: cartList,
+      [formDataSum]: newSummaryData.sum,
+    }));
+  }, [cartList, formDataField, formDataSum, setFormData]);
 
-	const closeModal = () => setIsModalOpen(false);
+  const closeModal = () => setIsModalOpen(false);
 
-	const decreaseQuantity = (e) => {
-		const selectedProductId = e.target.closest('li').id;
-		const selectedProduct = cartList.find(
-			(cartItem) => cartItem.id === selectedProductId
-		);
+  const decreaseQuantity = (e) => {
+    const selectedProductId = e.target.closest('li').id;
+    const selectedProduct = cartList.find(
+      (cartItem) => cartItem.id === selectedProductId
+    );
 
-		selectedProduct.quantity =
-			selectedProduct.quantity - 1 > 0 ? selectedProduct.quantity - 1 : 0;
+    selectedProduct.quantity =
+      selectedProduct.quantity - 1 > 0 ? selectedProduct.quantity - 1 : 0;
 
-		let updatedProducts;
+    let updatedProducts;
 
-		if (!selectedProduct.quantity) {
-			updatedProducts = cartList.filter(
-				(cartItem) => cartItem.id !== selectedProduct.id
-			);
-		} else {
-			updatedProducts = cartList.map((cartItem) =>
-				cartItem.id !== selectedProduct.id ? cartItem : selectedProduct
-			);
-		}
+    if (!selectedProduct.quantity) {
+      updatedProducts = cartList.filter(
+        (cartItem) => cartItem.id !== selectedProduct.id
+      );
+    } else {
+      updatedProducts = cartList.map((cartItem) =>
+        cartItem.id !== selectedProduct.id ? cartItem : selectedProduct
+      );
+    }
 
-		setCartList(updatedProducts);
-	};
+    setCartList(updatedProducts);
+  };
 
-	const increaseQuantity = (e) => {
-		const selectedProductId = e.target.closest('li').id;
-		const selectedProduct = cartList.find(
-			(cartItem) => cartItem.id === selectedProductId
-		);
+  const increaseQuantity = (e) => {
+    const selectedProductId = e.target.closest('li').id;
+    const selectedProduct = cartList.find(
+      (cartItem) => cartItem.id === selectedProductId
+    );
 
-		selectedProduct.quantity = selectedProduct.quantity + 1;
+    selectedProduct.quantity = selectedProduct.quantity + 1;
 
-		const updatedProducts = cartList.map((cartItem) =>
-			cartItem.id !== selectedProduct.id ? cartItem : selectedProduct
-		);
+    const updatedProducts = cartList.map((cartItem) =>
+      cartItem.id !== selectedProduct.id ? cartItem : selectedProduct
+    );
 
-		setCartList(updatedProducts);
-	};
+    setCartList(updatedProducts);
+  };
 
-	const isNextButtonDisabled =
-		type === 'home'
-			? formData.homeOfficeCart.length === 0
-			: formData.standardCart.length === 0;
+  const isNextButtonDisabled =
+    type === 'home'
+      ? formData.homeOfficeCart.length === 0
+      : formData.standardCart.length === 0;
 
-	return (
-		<div className={classes.productsFormContainer}>
-			<div className={classes.productsSummary}>
-				<div className={classes.summaryInfo}>
-					<h3 className={classes.summaryHeading}>
-						{window.innerWidth > 1024 && 'Oferta'} {headingText}
-					</h3>
-					<h4 className={classes.summarySubHeading}>
-						{subHeadingText} co miesiąc
-					</h4>
-				</div>
-				<div className={classes.summaryProducts}>
-					<div className={classes.summaryProduct}>
-						<span className={classes.productType}>KAWA ☕</span>
-						<span className={classes.productAmount}>x{summaryData.coffee}</span>
-					</div>
-					<div className={classes.summaryProduct}>
-						<span className={classes.productType}>HERBATA 🍵</span>
-						<span className={classes.productAmount}>x{summaryData.tea}</span>
-					</div>
-					<div className={classes.summaryProduct}>
-						<span className={classes.productType}>MATE 🧉</span>
-						<span className={classes.productAmount}>x{summaryData.mate}</span>
-					</div>
-				</div>
-				<p className={classes.summaryPrice}>
-					{summaryData.sum} PLN{type === 'home' && `/msc/os`}
-				</p>
-			</div>
-			<div className={classes.cartContainer}>
-				<h3 className={classes.cartHeading}>Koszyk &#128722;</h3>
-				<ul className={classes.cartList}>
-					{cartList.map((cartItem, i) => (
-						<li className={classes.cartListItem} id={cartItem.id} key={i}>
-							<img
-								alt={cartItem.name}
-								className={classes.productImg}
-								src={cartItem.photoBin}
-							/>
-							<div className={classes.productInfoContainer}>
-								<h4 className={classes.productName}>{cartItem.name}</h4>
-								<h5 className={classes.productDescription}>{cartItem.size}</h5>
-							</div>
-							<div className={classes.productControls}>
-								<span className={classes.minusSign} onClick={decreaseQuantity}>
-									-
-								</span>
-								<p className={classes.productQuantity}>{cartItem.quantity}</p>
-								<span className={classes.plusSign} onClick={increaseQuantity}>
-									+
-								</span>
-							</div>
-						</li>
-					))}
-					<li className={classes.addNewProductContainer}>
-						<FormButton
-							classes={classes}
-							onClick={() => setIsModalOpen(true)}
-							text="Dodaj Nowy Produkt +"
-						/>
-					</li>
-				</ul>
-			</div>
+  return (
+    <div className={classes.productsFormContainer}>
+      <div className={classes.productsSummary}>
+        <div className={classes.summaryInfo}>
+          <h3 className={classes.summaryHeading}>
+            {window.innerWidth >= 1280 && 'Oferta'} {headingText}
+          </h3>
+          <h4 className={classes.summarySubHeading}>
+            {subHeadingText} co miesiąc
+          </h4>
+        </div>
+        <div className={classes.summaryProducts}>
+          <div className={classes.summaryProduct}>
+            <span className={classes.productType}>KAWA ☕</span>
+            <span className={classes.productAmount}>x{summaryData.coffee}</span>
+          </div>
+          <div className={classes.summaryProduct}>
+            <span className={classes.productType}>HERBATA 🍵</span>
+            <span className={classes.productAmount}>x{summaryData.tea}</span>
+          </div>
+          <div className={classes.summaryProduct}>
+            <span className={classes.productType}>MATE 🧉</span>
+            <span className={classes.productAmount}>x{summaryData.mate}</span>
+          </div>
+        </div>
+        <p className={classes.summaryPrice}>
+          {summaryData.sum} PLN/msc{type === 'home' && `/os`}
+        </p>
+      </div>
+      <div className={classes.cartContainer}>
+        <h3 className={classes.cartHeading}>Koszyk &#128722;</h3>
+        <ul className={classes.cartList}>
+          {cartList.map((cartItem, i) => (
+            <li className={classes.cartListItem} id={cartItem.id} key={i}>
+              <img
+                alt={cartItem.name}
+                className={classes.productImg}
+                src={cartItem.photoBin}
+              />
+              <div className={classes.productInfoContainer}>
+                <h4 className={classes.productName}>{cartItem.name}</h4>
+                <h5 className={classes.productDescription}>{cartItem.size}</h5>
+              </div>
+              <div className={classes.productControls}>
+                <span className={classes.minusSign} onClick={decreaseQuantity}>
+                  -
+                </span>
+                <p className={classes.productQuantity}>{cartItem.quantity}</p>
+                <span className={classes.plusSign} onClick={increaseQuantity}>
+                  +
+                </span>
+              </div>
+            </li>
+          ))}
+          <li className={classes.addNewProductContainer}>
+            <FormButton
+              classes={classes}
+              onClick={() => setIsModalOpen(true)}
+              text='Dodaj Nowy Produkt +'
+            />
+          </li>
+        </ul>
+      </div>
 
-			<FormButton classes={classes} onClick={prevStep} text="Wróć" />
-			<FormButton
-				classes={classes}
-				disabled={isNextButtonDisabled}
-				onClick={nextStep}
-				text="Dalej"
-			/>
-			{isModalOpen && (
-				<AddProductModal
-					cartList={cartList}
-					onClose={closeModal}
-					products={productsList}
-					setCartList={setCartList}
-				/>
-			)}
-		</div>
-	);
+      <FormButton classes={classes} onClick={prevStep} text='Wróć' />
+      <FormButton
+        classes={classes}
+        disabled={isNextButtonDisabled}
+        onClick={nextStep}
+        text='Dalej'
+      />
+      {isModalOpen && (
+        <AddProductModal
+          cartList={cartList}
+          onClose={closeModal}
+          products={productsList}
+          setCartList={setCartList}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ProductsForm;
