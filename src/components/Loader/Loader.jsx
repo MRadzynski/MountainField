5618f17369f88627c1b5e89735ec284cdca9d354
MRadzynski <radzynski.milosz@gmail.com>
2022-04-05 20:24:27 +0200

    Add loader component fix commit

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import classes from '../../styles/components/loader.module.scss';
+
+const Loader = () => (
+  <div className={classes.loaderContainer}>
+    <div className={classes.loaderElement} />
+  </div>
+);
+
+export default Loader;
