d39af90a0a35f7579c91d7f86cbf4821fdb2efd4
MRadzynski <radzynski.milosz@gmail.com>
2022-04-06 23:02:51 +0200

    Upgrade React to 18.0.0 and add new sass colors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import './index.scss';
 import App from './App.jsx';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
